0fb5bcd32026a07b9b74719466e7f6a6ef0d6d4e
Anurag Roy <38952781+Anuragroyan@users.noreply.github.com>
2024-09-15 18:41:10 +0530

    this is a shopping cart app using flutter with form validation

---
+++ lib/data/dummy_items.dart
@@ -0,0 +1,51 @@
+import 'package:shopping_list_app/data/categories.dart';
+import 'package:shopping_list_app/models/category.dart';
+import 'package:shopping_list_app/models/grocery_item.dart';
+
+final groceryItems = [
+  GroceryItem(
+      id: 'a',
+      name: 'Milk',
+      quantity: 1,
+      category: categories[Categories.dairy]!),
+  GroceryItem(
+      id: 'b',
+      name: 'Bananas',
+      quantity: 5,
+      category: categories[Categories.fruit]!),
+  GroceryItem(
+      id: 'c',
+      name: 'Beef Steak',
+      quantity: 1,
+      category: categories[Categories.meat]!),
+  GroceryItem(
+      id: 'd',
+      name: 'Eggs',
+      quantity: 12,
+      category: categories[Categories.carbs]!),
+  GroceryItem(
+      id: 'e',
+      name: 'Red lentils',
+      quantity: 100,
+      category: categories[Categories.spices]!),
+  GroceryItem(
+      id: 'f',
+      name: 'Kaju barfi',
+      quantity: 50,
+      category: categories[Categories.sweets]!),
+  GroceryItem(
+      id: 'g',
+      name: 'Drycleaning of closthes',
+      quantity: 50,
+      category: categories[Categories.convenience]!),
+  GroceryItem(
+      id: 'h',
+      name: 'House cleaning',
+      quantity: 500,
+      category: categories[Categories.hygiene]!),
+  GroceryItem(
+      id: 'i',
+      name: 'Vehicles maintainance',
+      quantity: 200,
+      category: categories[Categories.other]!),
+];
